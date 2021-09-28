f61d34953b945312ab7af2d5166bf60e0ce08625
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-09-28 15:20:50 +0100

    Set local dev server

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'production',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
         template: './src/index.html',
